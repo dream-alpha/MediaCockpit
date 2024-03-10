edf739da029a372bb9210a853bf6aa77e2cb4c2a
dream-alpha <swmaniacster@gmail.com>
2024-03-10 19:07:08 +0100

    18.0.0

--- src/Version.py
+++ src/Version.py
@@ -21,6 +21,6 @@
 
 PLUGIN = "MediaCockpit"
 ID = "MDC"
-VERSION = "17.4.0"
+VERSION = "18.0.0"
 COPYRIGHT = "2018-2024 by dream-alpha"
 LICENSE = "This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version."
